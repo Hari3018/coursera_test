da76f93f4d37d07a7614fd8c2d49bef40b109f91
HARI PRASAD S <harirsmani2003@gmail.com>
2024-07-11 18:29:43 +0530

    deploy

--- mod4/script.js
+++ mod4/script.js
@@ -1,3 +1,26 @@
+(function(window){
+    var speakWord = "hello";
+    var hellospeaker = {
+        speak: function(names) {
+            console.log(speakWord + " " + names);
+        }
+    };
+    
+    window.hellospeaker = hellospeaker;
+})(window);
+
+(function(window){
+    var speakWord = "Good Bye";
+    var byespeaker = {
+        speak: function(names) {
+            console.log(speakWord + " " + names);
+        }
+    };
+    window.byespeaker = byespeaker;
+})(window);
+
+
+
 (function(){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) 
